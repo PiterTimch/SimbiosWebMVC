c78b74a82ec1c430b09f42152b406f4854070fe5
Petro <petrotymchuck@gmail.com>
2025-05-03 21:07:37 +0300

    done

---
+++ SimbiosWebMVC/Mapper/ProductMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SimbiosWebMVC.Data.Entities;
+using SimbiosWebMVC.Models.Category;
+using SimbiosWebMVC.Models.Product;
+
+namespace SimbiosWebMVC.Mapper
+{
+    public class ProductMapper : Profile
+    {
+        public ProductMapper()
+        {
+            CreateMap<ProductEntity, ProductItemViewModel>()
+                .ForMember(x => x.CategoryName, opt => opt.MapFrom(x => x.Category.Name))
+                .ForMember(x => x.Images, opt => opt.MapFrom(x => x.ProductImages.Select(x => x.Name)));
+
+            }
+    }
+}
